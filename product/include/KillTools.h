192dbe4f1e01fec10d0a397db7dd2f3d2a640f98
张小猿 <624518625@qq.com>
2016-08-18 14:47:52 +0800

    Update KillTools.h

--- product/include/KillTools.h
+++ product/include/KillTools.h
@@ -15,21 +15,21 @@ class KillTool
 public:
 	virtual ~KillTool();
 
-	static KillTool& GetInstance(int adp);
-	static KillTool* GetInstancePtr(int adp);
+	static KillTool& GetInstance(int adp, int personArr[]);
+	static KillTool* GetInstancePtr(int adp, int personArr[]);
 
 	//创建适配器
-	void CreateAdp(int adp);
+	void CreateAdp(int adp, int personArr[]);
 	KillAdp* GetAdpPtr();
 	//安装和卸载规则
-	void InstallRobertRule();
-	void InstallHumanRule();
-	void InstallMachineRule();
+	void InstallRobertRule(int personArr[]);
+	void InstallHumanRule(int personArr[]);
+	void InstallMachineRule(int personArr[]);
 	void UninstallRoberRule();
 	void UninstallHumanRule();
 	void UninstallMachineRule();
 private:
-	KillTool(int adpType);
+	KillTool(int adpType, int personArr[]);
 	KillAdp* m_AdpType;
 	static KillTool* m_pInstance;
 };
