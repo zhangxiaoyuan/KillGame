2fdf7b94eae1e0758d98d6eeac08cbc7a8069101
张小猿 <624518625@qq.com>
2016-08-18 14:49:11 +0800

    Update KillOrder.cpp

--- product/source/KillOrder.cpp
+++ product/source/KillOrder.cpp
@@ -3,18 +3,19 @@
 #include "KillOrder.h"
 #include "KillTools.h"
 
-KillOrder::KillOrder(string msg)
+KillOrder::KillOrder(string msg, int personArr[])
 {
 	int adpTtype = ParseMsg(msg);
 
 	//获取适配指针
-	m_killAdp = KillTool::GetInstancePtr(adpTtype)->GetAdpPtr();
+	m_killAdp = KillTool::GetInstancePtr(adpTtype, personArr)->GetAdpPtr();
 
 	/*将暗杀指令转换为对应的机器或者人的语言*/
 	m_language = m_killAdp->GetLanguage();
 	string name = m_language->GetLanName();
 
 	cout << "who: " << name << endl;
+	m_order = "order is " + name;
 } 
 
 KillOrder::~KillOrder()
@@ -51,65 +52,7 @@ void KillOrder::kill(int personId)
 
 }
 
-int KillOrder::PreGun(int id)
+string KillOrder::GetOrder()
 {
-	cout << "person[" << id << "]"  "gun pre action" << endl; 
-	return 0;
-}
-int KillOrder::DoGun(int id)
-{
-	cout << "person[" << id << "]" << "gun do action" << endl;
-	return 0;
-}
-int KillOrder::CleanGun(int id)
-{
-	cout << "person[" << id << "]"  "gun clean action" << endl;
-	return 0;
-}
-int KillOrder::AnswerGun(int id)
-{
-	cout << "person[" << id << "]"  "gun answer action" << endl;
-	return 0;
-}
-
-int KillOrder::PreKnife(int id)
-{
-	cout << "person[" << id << "]"  "knife pre action" << endl;
-	return 0;
-}
-int KillOrder::DoKnife(int id)
-{
-	cout << "person[" << id << "]"  "knife do action" << endl;
-	return 0;
-}
-int KillOrder::CleanKnife(int id)
-{
-	cout << "person[" << id << "]"  "knife clean action" << endl;
-	return 0;
-}
-int KillOrder::AnswerKnife(int id)
-{
-	cout << "person[" << id << "]"  "knife answer action" << endl;
-	return 0;
-}
-
-int KillOrder::PreRope(int id)
-{
-	cout << "person[" << id << "]"  "rope pre action" << endl;
-	return 0;
-}
-int KillOrder::DoRope(int id)
-{
-	cout << "person[" << id << "]"  "rope do action" << endl;
-	return 0;
-}
-int KillOrder::CleanRope(int id)
-{
-	cout << "person[" << id << "]"  "rope clean action" << endl;
-	return 0;
-}
-int KillOrder::AnswerRope(int id)
-{
-	cout << "person[" << id << "]" <<"rope answer action" << endl;
-	return 0;
+	return m_order;
 }
