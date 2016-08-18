edb231745250a8033fdc1926fd7937a2ce256f64
张小猿 <624518625@qq.com>
2016-08-18 14:47:20 +0800

    Update KillOrder.h

--- product/include/KillOrder.h
+++ product/include/KillOrder.h
@@ -13,32 +13,18 @@ class KillRule;
 class KillOrder
 {
 public:
-	KillOrder(string msg);
+	KillOrder(string msg, int personArr[]);
 	~KillOrder();
 
 	int ParseMsg(string msg);
-
-	int PreGun(int id);
-	int DoGun(int id);
-	int CleanGun(int id);
-	int AnswerGun(int id);
-
-	int PreKnife(int id);
-	int DoKnife(int id);
-	int CleanKnife(int id);
-	int AnswerKnife(int id);
-
-	int PreRope(int id);
-	int DoRope(int id);
-	int CleanRope(int id);
-	int AnswerRope(int id);
-
 	void kill(int perdon);
+	string GetOrder();
 
 private:
 	KillRule* m_killRule;
 	KillAdp*  m_killAdp;
 	Language* m_language;
+	string m_order;
 };
 
 #endif
